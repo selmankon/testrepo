45cb23734af1c6806140492eb55b8a4fa33d8638
selmankon <12685802+selmankon@users.noreply.github.com>
2023-05-05 14:37:59 +0300

    Create hey.py

---
+++ hey.py
@@ -0,0 +1,7 @@
+with open('creds.csv', 'r') as f:
+    for line in f:
+        email = line.split(',')[0]
+        print(email)
+
+aslasdljdfgfhkoypepwpcbkkerotxcbmasdg = "flag{whatareyoutalkingabout}"
+
